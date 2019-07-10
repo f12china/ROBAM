310eb31b5c9e9bbf78a39719cdffc1e7a124f266
f12china <570235525@qq.com>
2019-07-10 11:29:31 +0800

    数据渲染

--- robam/src/script/js/homedata.js
+++ robam/src/script/js/homedata.js
@@ -7,15 +7,28 @@
             let htmlstr = '';
             // console.log(product);
             $(product).each(function(i, e) {
-                console.log(e);
-                // let pic = JSON.parse(e.pic)
+                console.log(e)
+                    // let pic = JSON.parse(e.pic)
                 htmlstr += `
-               
+                <div class="charu">
+                <li class="productTab ">
+                 
+                    <!-- 活动小标签 -->
+                    <a href="script:; ">
+                        <div class="pic fl "><img class="lazy" data-original="${e.pic}"></div>
+                        <div class="con fr ">
+                            <h2>${e.title}</h2>
+                            <p>${e.donation}</p>
+                            <div class="money "> 特惠价：¥<span class="yuan ">${e.price}</span></div>
+                            <div class="more ">立即购买</div>
+                        </div>
+                    </a>
+                </li>
+            </div>
                 `
-
-
             });
-            $('.m-content-list').html(htmlstr);
+            $('.wel-limit-list').html(htmlstr);
+
             $(function() {
                 $("img.lazy").lazyload({
                     effect: "fadeIn"
